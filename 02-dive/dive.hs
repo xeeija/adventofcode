ac1aa86033ca873a573a7299ecab86845e24aac9
Xeeija <19950592+xeeija@users.noreply.github.com>
2021-12-02 20:51:33 +0100

    Solve 2nd puzzle

---
+++ 02-dive/dive.hs
@@ -0,0 +1,37 @@
+module Main where
+
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  f <- readFile (head (filter (\(x : xs) -> x /= '-') args))
+
+  print $ choose args (lines f)
+
+choose :: [String] -> [String] -> Int
+choose args
+  | "-2" `elem` args = dive'
+  | otherwise = dive
+
+dive :: [String] -> Int
+dive = uncurry (*) . foldr (move . words) (0, 0)
+  where
+    move :: [String] -> (Int, Int) -> (Int, Int)
+    move ("forward" : n : _) (x, y) = (x + read n, y)
+    move ("down" : n : _) (x, y) = (x, y + read n)
+    move ("up" : n : _) (x, y) = (x, y - read n)
+
+-- >>> dive $ lines "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"
+-- 150
+
+dive' :: [String] -> Int
+dive' = (\(x, y, a) -> x * y) . foldl move (0, 0, 0) . map words
+  where
+    move :: (Int, Int, Int) -> [String] -> (Int, Int, Int)
+    move (x, y, a) ("forward" : n : _) = (x + read n, y + (a * read n), a)
+    move (x, y, a) ("down" : n : _) = (x, y, a + read n)
+    move (x, y, a) ("up" : n : _) = (x, y, a - read n)
+
+-- >>> dive' $ lines "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"
+-- 900
