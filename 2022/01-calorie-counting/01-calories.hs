6a2ca6b242875703813e5ad1281ac28c08bb1e9f
Xeeija <19950592+xeeija@users.noreply.github.com>
2022-12-01 23:11:37 +0100

    optimize day 1: calories

--- 2022/01-calorie-counting/01-calories.hs
+++ 2022/01-calorie-counting/01-calories.hs
@@ -1,11 +1,11 @@
-import Data.List (groupBy)
+import Data.List.Split (splitOn)
 import Data.Maybe (mapMaybe)
 import Text.Read (readMaybe)
 
 main = interact calories
 
 calories :: String -> String
-calories = show . maximum . map (sum . mapMaybe readMaybe) . groupBy (\x y -> y /= "") . lines
+calories = show . maximum . map (sum . mapMaybe readMaybe . lines) . splitOn "\n\n"
 
 -- >>> calories "1000\n2000\n3000\n\n4000"
 -- "6000"
--- 2022/01-calorie-counting/01-calories3.hs
+++ 2022/01-calorie-counting/01-calories3.hs
@@ -1,4 +1,5 @@
 import Data.List (groupBy, sortOn)
+import Data.List.Split (splitOn)
 import Data.Maybe (mapMaybe)
 import Data.Ord (Down (Down))
 import Text.Read (readMaybe)
@@ -6,7 +7,7 @@ import Text.Read (readMaybe)
 main = interact calories3
 
 calories3 :: String -> String
-calories3 = show . sum . foldl top3 [0] . map (sum . mapMaybe readMaybe) . groupBy (\x y -> y /= "") . lines
+calories3 = show . sum . foldl top3 [0] . map (sum . mapMaybe readMaybe . lines) . splitOn "\n\n"
   where
     top3 acc x
       | x > minimum acc = take 3 $ sortOn Down (x : acc)
