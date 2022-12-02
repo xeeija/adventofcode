cbc3aad73fd21f85aec0add69878af3cf0448dbc
Xeeija <19950592+xeeija@users.noreply.github.com>
2022-12-02 12:47:20 +0100

    optimize day 2: rock paper scissors

--- 2022/02-rock-paper-scissors/02-rps.hs
+++ 2022/02-rock-paper-scissors/02-rps.hs
@@ -1,5 +1,5 @@
 main :: IO ()
-main = interact $ show . rps
+main = interact $ show . (\x -> (rps x, rps' x))
 
 rps :: String -> Int
 rps = sum . map play . lines
@@ -13,7 +13,18 @@ play (a : _ : b : _)
 score :: Char -> Int
 score = (`mod` 23) . flip (-) 64 . fromEnum
 
+rps' :: String -> Int
+rps' = sum . map play' . lines
+
+play' :: [Char] -> Int
+play' (a : _ : 'X' : _) = if l > 3 then l - 3 else l where l = score a + 2
+play' (a : _ : 'Y' : _) = 3 + score a
+play' (a : _ : 'Z' : _) = 6 + if w > 3 then w - 3 else w where w = score a + 1
+
+-- AX = Rock, BY = Paper, CZ = Scissors
+
 -- >>> rps "A Y\nB X\nC Z"
 -- 15
 
--- AX = Rock, BY = Paper, CZ = Scissors
+-- >>> rps' "A Y\nB X\nC Z"
+-- 12
