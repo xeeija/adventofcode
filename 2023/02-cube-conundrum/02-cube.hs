e548a46ce3eaf0394fcb8df1e1cad4fca3c165b0
Xeeija <19950592+xeeija@users.noreply.github.com>
2023-12-03 23:32:05 +0100

    solve day 2: cube-conundrum 2023

---
+++ 2023/02-cube-conundrum/02-cube.hs
@@ -0,0 +1,51 @@
+import Data.List.Split (splitOn)
+import Data.List (groupBy, sortOn, maximumBy)
+import Data.Function (on)
+
+type Game = [[Pick]]
+type Pick = (Integer, String)
+
+main = interact $ (++ "\n") . show . (\x -> (solve x, solve2 x))
+
+solve :: String -> Integer
+solve = sum . map fst . filter (all (all valid) . snd) . map parse . lines
+
+parse :: String -> (Integer, Game)
+parse = (\(id : game : _) -> (read (drop 5 id), parseGame game)) . splitOn ": "
+
+valid :: Pick -> Bool
+-- validPick :: (Ord a, Num a) => (a, String) -> Bool
+valid (x, "red") = x <= 12
+valid (x, "green") = x <= 13
+valid (x, "blue") = x <= 14
+valid (x, _) = False
+
+parseGame :: String -> Game
+parseGame = map (map ((\(x:color) -> (read x, head color)) . splitOn " ") . splitOn ", ") . splitOn "; "
+
+-- Part Two
+
+solve2 :: String -> Integer
+solve2 = sum . map (product . minCubes . parse) . lines
+
+minCubes :: (Integer, Game) -> [Integer]
+minCubes = map (fst . maximumBy (compare `on` fst)) . groupBy ((==) `on` snd) . sortOn snd . concat . snd
+
+-- >>> solve "Game 1: 13 green, 3 blue; 3 red, 1 green; 4 green, 3 red, 5 blue\nGame 2: 11 green, 3 blue; 3 red, 1 green; 4 green, 3 red, 5 blue"
+-- 3
+
+-- >>> all (all valid) [[(13,"red"),(1,"green")],[(4,"green"),(3,"red"),(5,"blue")]]
+-- False
+
+-- >>> parseGame "6 green, 3 blue; 3 red, 1 green; 4 green, 3 red, 5 blue"
+-- [[(6,"green"),(3,"blue")],[(3,"red"),(1,"green")],[(4,"green"),(3,"red"),(5,"blue")]]
+
+
+-- minCubes2 :: String -> Integer
+-- minCubes2 = product . map (fst . maximumBy (compare `on` fst)) . concatMap (groupBy ((==) `on` snd) . sortOn snd . concat . snd . parse) . lines
+
+-- >>> minCubes "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green" -- \nGame 2: 11 green, 3 blue; 3 red, 1 green; 4 green, 3 red, 5 blue"
+-- 48
+
+-- >>> solve2 $ "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
+-- 60
