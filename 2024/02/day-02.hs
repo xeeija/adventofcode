cfff8d5dd2ca49e7bc133ba695e4f35b581e4219
Xeeija <19950592+xeeija@users.noreply.github.com>
2024-12-02 12:13:28 +0100

    2024: solve day 2

---
+++ 2024/02/day-02.hs
@@ -0,0 +1,37 @@
+import Control.Applicative (liftA2)
+import Data.List (subsequences)
+
+main :: IO ()
+main = interact $ (++ "\n") . show . liftA2 (,) solve solve2
+
+solve :: String -> Int
+solve = length . filter id . map (safe . diff) . parse
+
+parse :: String -> [[Integer]]
+parse = map (map read . words) . lines
+
+diff :: Num c => [c] -> [c]
+diff xs = zipWith subtract xs (tail xs)
+
+safe :: (Ord a, Num a) => [a] -> Bool
+safe xs
+  | head xs > 0 = all (\x -> x > 0 && x <= 3) xs
+  | head xs <= 0 = all (\x -> x < 0 && x >= -3) xs
+
+-- Part Two
+
+solve2 :: String -> Int
+solve2 = length . filter id . map (any (safe . diff) . dampener) . parse
+
+dampener :: [a] -> [[a]]
+dampener xs = (filter ((== (length xs - 1)) . length) . subsequences) xs
+
+
+-- solve2 = length . filter id . map (any (null . safe' . diff) . dampener) . parse
+
+-- safe' :: (Ord a, Num a) => [a] -> [a]
+-- safe' xs
+--   | head xs > 0 = filter (\x -> x <= 0 || x > 3) xs
+--   | head xs < 0 = filter (\x -> x > 0 || x < -3) xs
+--   | head xs == 0 = xs
+
