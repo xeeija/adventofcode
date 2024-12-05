4f484b2f57bd67faab4c8ee788c5b3c24883a03e
Xeeija <19950592+xeeija@users.noreply.github.com>
2024-12-05 15:38:22 +0100

    2024: update day 4 golf solution

--- 2024/04/day-04-golf.hs
+++ 2024/04/day-04-golf.hs
@@ -1,14 +1,25 @@
-import Control.Monad;import Data.List;l=length;m a b@(x,y,z)|o[ä x,w y,ö z]=a#b|
- o[ö x,w y,ä z]=a#b|o[ü x,w y,ü z]=a#b|o[ß x,w y,ß z]=a#b;m a(_:x,_:y,_:z)|l x<2
- =a|1>0=m a(x,y,z);main=interact$show.liftM2(,)(e.n(e.n(c 0)).p)(e.n(m 0).(zip3
- <*>r<*>drop 2).k);d=u.zipWith(\i x->replicate i ' '++x)[0..];(#)a(x,y,z)=m(a+1)
- (r x,r y,r z);r=drop 1;p=liftM4(\a b c d->[a,b,c,d])id v d(d.v).k;c a s|"XMAS"%
- s||"SAMX"%s=c(a+1)$r s|l s<4=a|1>0=c a$r s;(%)=isPrefixOf;o=all(>0);v=n reverse
- .u;ä('M':_:'M':_)=1;ä _=0;ö('S':_:'S':_)=1;ö _=0;ü('M':_:'S':_)=1;ü _=0
-ß('S':_:'M':_)=1;ß _=0;w(_:'A':_)=1;w _=0;u=transpose;n=map;e=sum;k=lines
+import GHC.Base;import Data.List;l=length;q=drop
+m a b@(x,y,z)|o[ä x,w y,ö z]||o[ö x,w y,ä z]||o[ü x,w y,ü z]||o[ß x,w y,ß z]=a#b
+m a(_:x,_:y,_:z)|l x<2=a|1>0=m a(x,y,z);main=interact$show.liftM2(,)(e.n(e.n(c
+ 0)).p)(e.n(m 0).(zip3<*>r<*>q 2).k);d=u.zipWith(\i x->replicate i ' '++x)[0..]
+(#)a(x,y,z)=m(a+1)(r x,r y,r z);r=q 1;p=liftM4(\a b c d->[a,b,c,d])id v d(d.v).k
+c a s|"XMAS"%s||"SAMX"%s=c(a+1)$r s|l s<4=a|1>0=c a$r s;(%)=isPrefixOf;o=all(>0)
+v=n reverse.u;ä('M':_:'M':_)=1;ä _=0;ö('S':_:'S':_)=1;ö _=0;ü('M':_:'S':_)=1
+ü _=0;ß('S':_:'M':_)=1;ß _=0;w(_:'A':_)=1;w _=0;u=transpose;n=map;e=sum;k=lines
 
 
 -- fits on an IBM 5081 Punch Cards (10 rows, 80 columns)
+
+
+-- import Control.Monad;import Data.List;l=length;m a b@(x,y,z)|o[ä x,w y,ö z]=a#b|
+--  o[ö x,w y,ä z]=a#b|o[ü x,w y,ü z]=a#b|o[ß x,w y,ß z]=a#b;m a(_:x,_:y,_:z)|l x<2
+--  =a|1>0=m a(x,y,z);main=interact$show.liftM2(,)(e.n(e.n(c 0)).p)(e.n(m 0).(zip3
+--  <*>r<*>drop 2).k);d=u.zipWith(\i x->replicate i ' '++x)[0..];(#)a(x,y,z)=m(a+1)
+--  (r x,r y,r z);r=drop 1;p=liftM4(\a b c d->[a,b,c,d])id v d(d.v).k;c a s|"XMAS"%
+--  s||"SAMX"%s=c(a+1)$r s|l s<4=a|1>0=c a$r s;(%)=isPrefixOf;o=all(>0);v=n reverse
+--  .u;ä('M':_:'M':_)=1;ä _=0;ö('S':_:'S':_)=1;ö _=0;ü('M':_:'S':_)=1;ü _=0
+-- ß('S':_:'M':_)=1;ß _=0;w(_:'A':_)=1;w _=0;u=transpose;n=map;e=sum;k=lines
+
 
 
 -- c a s@('X':'M':'A':'S':_)=c (a+1)$r s
