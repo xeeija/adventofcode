519b07dc732477c596eb59393d57e5e415c405a0
Xeeija <19950592+xeeija@users.noreply.github.com>
2024-12-08 23:58:36 +0100

    2024: simplify day 8

--- 2024/08/day-08.hs
+++ 2024/08/day-08.hs
@@ -9,32 +9,29 @@ main = interact $ (++ "\n") . show . liftA2 (,) solve solve2
 
 
 solve :: String -> Int
-solve = length . signals antinodes . lines
--- solve = length . nub . (\x -> concatMap (allAntinodes (length x, length (head x)) . (`antennas` x)) ['0'..'z']) . lines
+solve = length . signals antinodes' . lines
 
 -- signals :: (Eq a, Num n, Enum n) => ((Int, Int) -> [(n, Int)] -> [a]) -> [[Char]] -> [a]
 signals :: (Eq a) => (Point -> [Point] -> [a]) -> [String] -> [a]
-signals fn = nub . (\x -> concatMap (fn (length x, length (head x)) . (`antennas` x)) ['0'..'z'])
+signals fn xs = nub $ concatMap (fn (bounds xs) . (`antennas` xs)) ['0'..'z']
+  where
+    bounds x = (length x, length (head x))
 
 -- antennas :: (Num a1, Enum a1, Eq a2) => a2 -> [[a2]] -> [(a1, Int)]
 antennas :: (Eq a) => a -> [[a]] -> [Point]
 antennas x = concatMap (uncurry (zip . repeat)) . filter (not . null . snd) . zip [0..] . map (elemIndices x)
 -- antennas x = concatMap (\(x,xs) -> zip (repeat x) xs) . filter (not . null . snd) . zip [0..] . map (elemIndices x)
 
--- antinodes :: (Ord a, Num a) => (a, b) -> [(a, a)] -> [(a, a)]
-antinodes :: Point -> [Point] -> [Point]
-antinodes max xs = concatMap (\a -> filter (valid max) (map (antinode . (a,)) (filter (/= a) xs))) xs
+antinodes' :: Point -> [Point] -> [Point]
+antinodes' max xs = filter (valid max) [antinode' a b | a <- xs, b <- xs, a /= b]
+
+antinode' :: Point -> Point -> Point
+antinode' a b = a +: (b -: a)
 
 -- valid :: (Ord a, Num a) => (a, b) -> (a, a) -> Bool
 valid :: Point -> Point -> Bool
 valid (rMax, cMax) (r, c) = and [r >= 0, r < rMax, c >= 0, c < rMax]
 
-
--- antinode :: (Num a, Num b) => ((a, b), (a, b)) -> (a, b)
-antinode :: (Point, Point) -> Point
-antinode (a, b) = a +: (b -: a)
--- antinode (a, b) = a +: distance b a
--- antinode'' (a,b) = (a, distance b a, a +: distance b a)
 
 (-:) :: Point -> Point -> Point
 (-:) (r1, c1) (r2, c2) = (r2 - r1, c2 - c1)
@@ -47,21 +44,46 @@ antinode (a, b) = a +: (b -: a)
 -- Part Two
 
 solve2 :: String -> Int
-solve2 = length . signals resonantAntinodes . lines
+solve2 = length . signals resonantAntinodes' . lines
 
--- resonantAntinodes :: (Ord a, Num a) => (a, b) -> [(a, a)] -> [(a, a)]
-resonantAntinodes :: Point -> [Point] -> [Point]
-resonantAntinodes max xs = concatMap (\a -> concatMap (takeWhile (valid max) . resonantNodes . (a,)) (filter (/= a) xs)) xs
+resonantAntinodes' :: Point -> [Point] -> [Point]
+resonantAntinodes' max xs = concat [nodes a b | a <- xs, b <- xs, a /= b]
+  where nodes a b = takeWhile (valid max) (resonantNodes' a b)
 
--- resonantNodes :: (Num a, Num b) => ((a, b), (a, b)) -> [(a, b)]
-resonantNodes :: (Point, Point) -> [Point]
-resonantNodes (a, b) = iterate (+: (b -: a)) a
+resonantNodes' :: Point -> Point -> [Point]
+resonantNodes' a b = iterate (+: (b -: a)) a
 
 -- "resonantNodes" is a superset for "antinode" from part 1
 -- antinode == (head . tail . resonantNodes)
 
 
 -- Unused
+
+-- equivalent to "signal" above, but with list comprehension
+-- signals' :: (Eq a) => (Point -> [Point] -> [a]) -> [String] -> [a]
+-- signals' fn xs = nub $ concat [fn bounds (antennas fr xs) | x <- xs, fr <- ['0'..'z']]
+--   where
+--     bounds = (length xs, length (head xs))
+
+-- antinodes :: (Ord a, Num a) => (a, b) -> [(a, a)] -> [(a, a)]
+-- antinodes :: Point -> [Point] -> [Point]
+-- antinodes max xs = concatMap (\a -> filter (valid max) (map (uncurry antinode' . (a,)) (filter (/= a) xs))) xs
+
+-- antinode :: (Num a, Num b) => ((a, b), (a, b)) -> (a, b)
+-- antinode :: (Point, Point) -> Point
+-- antinode (a, b) = a +: (b -: a)
+-- antinode (a, b) = a +: distance b a
+-- antinode'' (a,b) = (a, distance b a, a +: distance b a)
+
+-- resonantAntinodes :: (Ord a, Num a) => (a, b) -> [(a, a)] -> [(a, a)]
+-- resonantAntinodes :: Point -> [Point] -> [Point]
+-- resonantAntinodes max xs = concatMap (\a -> concatMap (takeWhile (valid max) . resonantNodes . (a,)) (filter (/= a) xs)) xs
+
+-- -- resonantNodes :: (Num a, Num b) => ((a, b), (a, b)) -> [(a, b)]
+-- resonantNodes :: (Point, Point) -> [Point]
+-- resonantNodes (a, b) = iterate (+: (b -: a)) a
+
+
 
 -- distance :: (Num a, Num b) => (a, b) -> (a, b) -> (a, b)
 -- distance :: Point -> Point -> Point
