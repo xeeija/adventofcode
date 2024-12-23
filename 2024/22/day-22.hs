a63271f6223b9ad936e047101bb860345fb122c2
Xeeija <19950592+xeeija@users.noreply.github.com>
2024-12-23 19:31:04 +0100

    2024: solve day 22 (part 2)

--- 2024/22/day-22.hs
+++ 2024/22/day-22.hs
@@ -1,8 +1,11 @@
 import GHC.Base (liftA2)
 import Data.Bits
+import Data.List (zipWith4)
+import qualified Data.Map as M
+import qualified Data.Set as S
 
 main :: IO ()
-main = interact $ (++ "\n") . show . liftA2 (,) solve (const 0)
+main = interact $ (++ "\n") . show . liftA2 (,) solve solve2
 
 solve :: String -> Int
 solve = sum . map (day secret . read) . lines
@@ -23,4 +26,49 @@ mix b n = (n .^. shift n b) .&. 0xffffff
 
 day :: (c -> c) -> c -> c
 day f = (!! 2000) . iterate f
+
+
+-- Part Two
+
+
+-- solve2 :: String -> (S.Set [Int], [M.Map [Int] Int])
+solve2 :: String -> (Int, [Int])
+solve2 = liftA2 bestChange id (S.unions . map M.keysSet) . map (changesMap 2000 . read) . lines
+
+
+bestChange :: (Foldable t, Num v, Ord v, Ord k) => [M.Map [k] v] -> t [k] -> (v, [k])
+bestChange maps = foldl findBest (0, [])
+  where
+    findBest acc@(accVal, _) k = if total k > accVal then (total k, k) else acc
+    total k = sum $ map (M.findWithDefault 0 k) maps
+
+-- fold: look at the prices of all buyers for every tuple of changes
+-- and set it as new max if it has more total bananas then previous
+
+
+price :: Int -> [Int]
+price = map (`mod` 10) . iterate secret
+
+diff :: Num a => [a] -> [a]
+diff = zipWith subtract <*> drop 1
+
+changes :: [a] -> [[a]]
+changes = zipWith4 (\a b c d -> [a,b,c,d]) <*> drop 1 <*> drop 2 <*> drop 3
+-- changes = zip4 <*> drop 1 <*> drop 2 <*> drop 3
+
+-- changes' xs = zipWith4 (\a b c d -> [a,b,c,d]) xs (drop 1 xs) (drop 2 xs) (drop 3 xs)
+
+priceChanges :: Int -> [(Int, [Int])]
+priceChanges = liftA2 zip id (changes . diff) . price
+-- priceChanges = liftA2 zip price (changes . diff . price)
+
+priceChanges' :: Int -> [([Int], Int)]
+priceChanges' = liftA2 zip (changes . diff) (drop 4) . price
+-- equivalent to:
+
+
+changesMap :: Int -> Int -> M.Map [Int] Int
+changesMap n = M.fromListWith (\a b -> b) . (take n . priceChanges')
+
+-- (\a b -> b) == flip const == const id
 
